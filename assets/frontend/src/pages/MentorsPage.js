23694479e2f0148175c1daf605a0069d000c1739
Eszti <nagy.eszter2222@gmail.com>
2026-01-23 13:58:06 +0100

    css import

--- assets/frontend/src/pages/MentorsPage.js
+++ assets/frontend/src/pages/MentorsPage.js
@@ -3,6 +3,8 @@ import Mentor from "../components/Mentor";
 import { MentorContext } from "../contexts/MentorContext";
 import { AuthContext } from "../contexts/AuthContext";
 
+import "../css/mentor.css";
+
 export default function MentorsPage() {
   const { mentorList, getMentor } = useContext(MentorContext);
   const { user, loadUser, loading } = useContext(AuthContext);
